760c42f5515d7d098e850b71ef7aa98c17eb8939
Miguel Ferreira <miguel.studies1011@gmail.com>
2023-01-20 21:19:30 -0300

    Clean archtecture structure

--- taks_management_app/lib/main.dart
+++ taks_management_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:taks_management_app/utils/theme.dart';
+import 'package:taks_management_app/theme.dart';
 
 void main() {
   runApp(const MyApp());
